bc376e5efb06f0e2d9b60cf7b4d2fbbd563c4984
azadov-azamat <azamatazadov112@gmail.com>
2025-08-17 17:40:36 +0500

    refactor(booking-page): added breadcrumb

--- src/pages/booking/index.tsx
+++ src/pages/booking/index.tsx
@@ -1,11 +1,20 @@
+import Breadcrumb from "../../components/breadcrumb";
 import HeaderCard from "../../components/card/header"
 import NewsSlider from "../../components/slider/news";
 
 function Booking() {
+
+  const breadcrumbItems = [
+    { label: "Главная", href: "/" },
+    { label: "Бронирование" }
+  ];
+
   return (
     <main className="mt-8 mb-16 space-y-16">
       <HeaderCard>
-
+        <div className="flex  items-center justify-between my-10">
+          <Breadcrumb items={breadcrumbItems} />
+        </div>
         <div>
           <img
               src="/images/leaf/1.png"
