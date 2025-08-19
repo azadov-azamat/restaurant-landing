c064e9c7c8e75561ac8184cbe28645d7adcf0da8
azadov-azamat <azamatazadov112@gmail.com>
2025-08-19 20:40:30 +0500

    fix(header-card): fixed min height

--- src/components/card/header.tsx
+++ src/components/card/header.tsx
@@ -9,7 +9,7 @@ type HeaderCardProps = {
 export default function HeaderCardComponent({ children }: HeaderCardProps) {
   return (
     <div
-      className="relative w-full rounded-3xl bg-white/50 backdrop-blur-[10px]
+      className="relative w-full rounded-3xl bg-white/50 backdrop-blur-[10px] min-h-[450px]
                  md:p-8 lg:px-16 lg:pt-4 lg:pb-10 p-4 text-white "
     >
       <Navigation navLinks={NAVIGATION_LINKS} />
